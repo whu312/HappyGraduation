7d4e6677e4d6dae5ea6fe64c6fc42a4cad389100
loyoen <loyoen@gmail.com>
2016-04-20 13:25:30 +0800

    add tables in model

--- HG/website/models.py
+++ HG/website/models.py
@@ -8,3 +8,38 @@ class users(models.Model):
     name = models.CharField(max_length = 128)
     jurisdiction = models.IntegerField()
     thisuser = models.ForeignKey(User)
+class cycle(models.Model):
+    name = models.CharField(max_length = 128)
+class product(models.Model):
+    name = models.CharField(max_length = 128)
+    rate = models.CharField(max_length = 16)
+    repaycycle = models.ForeignKey(cycle)
+    closedtype = models.CharField(max_length = 16)
+    closedperiod = models.IntegerField()
+class field(models.Model):
+    name = models.CharField(max_length = 128)
+    address = models.CharField(max_length = 128)
+    tel = models.CharField(max_length = 128)
+class party(models.Model):
+    name = models.CharField(max_length = 128)
+    thisfield = models.ForeignKey(field)
+class manager(models.Model):
+    name = models.CharField(max_length = 128)
+    tel = models.CharField(max_length = 32)
+    number = models.CharField(max_length = 128)
+    thisparty = models.ForeignKey(party) 
+class contract(models.Model):
+    number = models.CharField(max_length = 128)
+    client_name = models.CharField(max_length = 128)
+    client_idcard = models.CharField(max_length = 128)
+    bank = models.CharField(max_length = 128)
+    bank_card = models.CharField(max_length = 128)
+    money = models.CharField(max_length = 128)
+    thisproduct = models.ForeignKey(product)
+    startdate = models.CharField(max_length = 32)
+    enddata = models.CharField(max_length = 32)
+    status = models.IntegerField()
+    thismanager = models.ForeignKey(manager)
+class loginfo(models.Model):
+    info = models.CharField(max_length = 1024)
+    thisuser = models.ForeignKey(users)
